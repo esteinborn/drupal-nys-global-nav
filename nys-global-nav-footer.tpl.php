d6586b26b84132618019d25042d9af03d0f5cf47
Gregg Marshall <gmarshall@vendor-tech.com>
2015-12-23 06:10:47 -0700

    Refactored to use .tpl files

---
+++ nys-global-nav-footer.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @file
+ * New York State Global Navigation Header.
+ * See https://github.com/ny/global-navigation for documentation.
+ *
+ * Variables:
+ * - none.
+ *
+ * @ingroup themeable
+ */
+?>
+<h1 class="nys-global-header">Global Header Goes Here -- <?php print variable_get('nys_global_nav_agency_color', 'administration') ?></h1>
