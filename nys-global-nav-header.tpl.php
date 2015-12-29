98c377522dda77c62012c659b924d0436f77aa0e
Gregg Marshall <gregg.marshall@its.ny.gov>
2015-12-29 10:09:05 -0500

    Added nys-global-header id to template

--- nys-global-nav-header.tpl.php
+++ nys-global-nav-header.tpl.php
@@ -12,7 +12,7 @@
  * @ingroup themeable
  */
 ?>
-<div class="nys-global-header <?php print variable_get('nys_global_nav_header_format', ''); ?>">
+<div id="nys-global-header" class="nys-global-header <?php print variable_get('nys_global_nav_header_format', ''); ?>">
   <div class="nav-toggle">
     <a href="#" role="button" id="nys-menu-control">Navigation menu</a>
   </div>
